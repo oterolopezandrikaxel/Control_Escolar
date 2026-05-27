408b9b557cc9b45be26f9462e6ba0dd1c77d6229
Otero <oterolopezandrikaxel@gmail.com>
2026-05-27 13:52:30 -0600

    Agregado módulo de Inscripciones

--- Inscripciones.js
+++ Inscripciones.js
@@ -1,2 +1,3 @@
 console.log("Hola, este es el archivo Inscripciones");
 console.log("Módulo de Alumnos - Inscripciones, reinscripciones y bajas");
+console.log("Cambio realizado desde Visual Studio Code");
